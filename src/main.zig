3b07b7de6851db972e1f2ae81022866c733a8255
Santiago Verdu <erra@qmake.org>
2023-11-28 02:32:50 -0700

    open

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,11 @@
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const file = try std.fs.cwd().openFile("arc.pcapng", .{});
+    defer file.close();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
 
-    try bw.flush(); // don't forget to flush!
+    try bw.flush();
 }
